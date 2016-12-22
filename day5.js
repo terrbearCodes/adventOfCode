e663087e969f5629152024cb8b1628770f78fd42
Terry Keeney II <terry@tkeeney.com>
2016-12-22 10:08:49 -0700

    fix ☠ mistake

--- day5.js
+++ day5.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const SparkMD5 = require("spark-md5");
 
 const sample = 'abc';
 const input = 'ojvtpuvg';
@@ -15,14 +16,14 @@ let incrementer = 1;
 //     incrementer++;
 // }
 
-let passCode = ['_','_','_','_','_','_','_','_'];
+let passCode = ['','','','','','','',''];
 
-while(passCode.indexOf('_') > -1) {
-    const hash = crypto.createHash('md5').update(`${input}${incrementer}`).digest("hex");
+while(passCode.indexOf('') > -1) {
+    const hash = SparkMD5.hash(`${input}${incrementer}`);
     if(hash.startsWith('00000')){
-        const passCodeIdx = parseInt(hash.charAt(5));
-        if(passCodeIdx && passCodeIdx < 8){
-            if(passCode[passCodeIdx] == '_'){
+        const passCodeIdx = parseInt(hash.charAt(5),16);
+        if(passCodeIdx < 8){
+            if(!passCode[passCodeIdx]){
                 passCode[passCodeIdx] = hash.charAt(6);
                 console.log(passCode);
             }

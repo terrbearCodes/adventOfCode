a8e78b38c28f38f14d44ca4e3fcafba5630d59b0
Terry Keeney II <terry@tkeeney.com>
2017-12-05 10:15:13 -0700

    part 2 ⭐️⭐️👌

--- 2017/day5.js
+++ 2017/day5.js
@@ -10,7 +10,7 @@ let test2 = '2 3 2 3 -1'.split(' ').map( v => parseInt(v) );
 
 let input = fs.readFileSync('day5_input.txt','utf8').split('\n').map( v => parseInt(v) ); //INPUT
 
-function travelMaze(instructions){
+function travelMaze(instructions, stepMod){
     let counter = 0;
     let pointer = 0;
 
@@ -19,12 +19,24 @@ function travelMaze(instructions){
         const currentPointer = pointer;
         pointer += instructions[pointer]; // where to jump to next
         // console.log(`NEW ${pointer}`)
-        instructions[currentPointer] = instructions[currentPointer] + 1; //increment instruction
+        instructions[currentPointer] = stepMod(instructions[currentPointer]); //increment instruction
 
         counter++;
     }
+    // console.log(instructions)
     return counter
 }
 
-console.log(`Test maze: ${travelMaze(testIn)}`);
-console.log(`PART 1: ${travelMaze(input)}`);
+function part1(value){
+    return value + 1
+}
+
+function part2(value){
+    return (value >= 3) ? value -1 : value + 1;
+}
+
+// console.log(`Test maze: ${travelMaze(testIn, part1)}`);
+// console.log(`PART 1: ${travelMaze(input, part1)}`);
+
+console.log(`Test Part2: ${travelMaze(testIn,part2)}`);
+console.log(`PART 2: ${travelMaze(input,part2)}`);
